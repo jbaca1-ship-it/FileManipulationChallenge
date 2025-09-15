a55f860f00eea375d55cb6549ce8bb427495a224
jbaca1-ship-it <joseph.p.baca8@gmail.com>
2025-09-15 13:07:12 -0700

    (FMC-0.4.2.2): Program now generates text files inside directory
    Generates "notes.txt," "data.txt," and "log.txt"

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
 
 public class FileHandlingActivity {
     public static void main(String[] args) {
@@ -10,7 +12,16 @@ public class FileHandlingActivity {
         dir.mkdir();
         
         // b. Create three text files
-        
+
+        try {
+            Files.write(Paths.get("notes.txt"), "Password".getBytes(StandardCharsets.UTF_8));
+            Files.write(Paths.get("data.txt"), "Password".getBytes(StandardCharsets.UTF_8));
+            Files.write(Paths.get("log.txt"), "Password".getBytes(StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
         // c. Write messages to files
         
         // d. Read and display file contents
