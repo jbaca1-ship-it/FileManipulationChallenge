1bbca380754bb0d4206f45fece22d07456fb5f53
jbaca1-ship-it <joseph.p.baca8@gmail.com>
2025-09-15 16:39:43 -0700

    (FMC-0.4.2.7): Program lists all files in both directories
    Program lists all files in both JavaFileSystem and Backup

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -8,15 +8,15 @@ public class FileHandlingActivity {
         
         // a. Create main directory
 
-        File dir = new File("main");
+        File dir = new File("JavaFileSystem");
         dir.mkdir();
         
         // b. Create three text files
 
         try {
-            Files.createFile(Paths.get("notes.txt"));
-            Files.createFile(Paths.get("data.txt"));
-            Files.createFile(Paths.get("log.txt"));
+            Files.createFile(Paths.get("JavaFileSystem/notes.txt"));
+            Files.createFile(Paths.get("JavaFileSystem/data.txt"));
+            Files.createFile(Paths.get("JavaFileSystem/log.txt"));
         } catch (Exception e) {
             // TODO: handle exception
         }
@@ -24,9 +24,9 @@ public class FileHandlingActivity {
         // c. Write messages to files
         
          try {
-            Files.write(Paths.get("notes.txt"), "Fortnite".getBytes(StandardCharsets.UTF_8));
-            Files.write(Paths.get("data.txt"), "Battle".getBytes(StandardCharsets.UTF_8));
-            Files.write(Paths.get("log.txt"), "Pass".getBytes(StandardCharsets.UTF_8));
+            Files.write(Paths.get("JavaFileSystem/notes.txt"), "Fortnite".getBytes(StandardCharsets.UTF_8));
+            Files.write(Paths.get("JavaFileSystem/data.txt"), "Battle".getBytes(StandardCharsets.UTF_8));
+            Files.write(Paths.get("JavaFileSystem/log.txt"), "Pass".getBytes(StandardCharsets.UTF_8));
         } catch (Exception e) {
             // TODO: handle exception
         }
@@ -34,9 +34,9 @@ public class FileHandlingActivity {
         // d. Read and display file contents
 
         try {
-            String notes = new String(Files.readAllBytes(Paths.get("notes.txt")));
-            String data = new String(Files.readAllBytes(Paths.get("data.txt")));
-            String log = new String(Files.readAllBytes(Paths.get("log.txt")));
+            String notes = new String(Files.readAllBytes(Paths.get("JavaFileSystem/notes.txt")));
+            String data = new String(Files.readAllBytes(Paths.get("JavaFileSystem/data.txt")));
+            String log = new String(Files.readAllBytes(Paths.get("JavaFileSystem/log.txt")));
             System.out.println("notes.txt: " + notes);
             System.out.println("data.txt: " + data);
             System.out.println("log.txt: "+log);
@@ -46,7 +46,7 @@ public class FileHandlingActivity {
         
         // e. Create backup directory
 
-        Path dirr = Paths.get(Paths.get("main").toString(), "Backup");
+        Path dirr = Paths.get(Paths.get("JavaFileSystem").toString(), "Backup");
         try {
             Files.createDirectories(dirr);
         } catch (Exception e) {
@@ -55,15 +55,32 @@ public class FileHandlingActivity {
         
         // f. Copy contents to backup file
         try {
-            String notes = new String(Files.readAllBytes(Paths.get("notes.txt")));
-            String data = new String(Files.readAllBytes(Paths.get("data.txt")));
-            String log = new String(Files.readAllBytes(Paths.get("log.txt")));
+            String notes = new String(Files.readAllBytes(Paths.get("JavaFileSystem/notes.txt")));
+            String data = new String(Files.readAllBytes(Paths.get("JavaFileSystem/data.txt")));
+            String log = new String(Files.readAllBytes(Paths.get("JavaFileSystem/log.txt")));
             String text = notes + data + log;
-            Files.write(Paths.get("main/Backup").resolve(Paths.get("backup.txt")), text.getBytes(StandardCharsets.UTF_8));
+            Files.write(Paths.get("JavaFileSystem/Backup").resolve(Paths.get("backup.txt")), text.getBytes(StandardCharsets.UTF_8));
         } catch (Exception e) {
             // TODO: handle exception
         }
         
         // g. List all files in both directories
+        try {
+            String str = new String();
+            File main = new File("JavaFileSystem");
+            File backup = new File("JavaFileSystem/Backup");
+            str += "Contains:\n";
+            for (File file : main.listFiles()) {
+                if (file.toString().contains(".txt")) {
+                    str += file + "\n";
+                }                  
+            }
+            for (File file : backup.listFiles()) {
+                str += file + "\n";
+            }
+            System.out.println(str.trim());
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
 }
